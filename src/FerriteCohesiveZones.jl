fa274d16020ff33e0772a882136c4c46ee630951
kimauth <auth@chalmers.se>
2022-09-25 19:10:03 +0200

    exports

--- src/FerriteCohesiveZones.jl
+++ src/FerriteCohesiveZones.jl
@@ -7,4 +7,9 @@ include("cohesive_cell.jl")
 include("cohesive_interpolations.jl")
 include("cohesive_values.jl")
 
+export CohesiveQuadrilateral, QuadraticCohesiveQuadrilateral
+export JumpInterpolation, MidPlaneInterpolation
+export CohesiveVectorValues
+export get_rotation, getdetJdA
+
 end
