9ef541abd194c5919da4ef773407fab91db090c0
termi-official <termi-official@users.noreply.github.com>
2023-04-21 18:14:33 +0200

    Hotfix.

--- src/cohesive_interpolations.jl
+++ src/cohesive_interpolations.jl
@@ -11,6 +11,8 @@ end
 
 nvertices(ip::Interpolation) = length(Ferrite.vertexdof_indices(ip))
 nvertexdofs(ip::Interpolation) = sum(length.(Ferrite.vertexdof_indices(ip)); init=0)
+nfacedofs(ip::Interpolation) = sum(length.(Ferrite.facedof_indices(ip)); init=0)
+ncelldofs(ip::Interpolation) = length(Ferrite.celldof_interior_indices(ip))
 
 JumpInterpolation(ip::Interpolation{dim,shape,order}) where {dim,shape,order} = JumpInterpolation{dim+1,shape,order,typeof(ip)}(ip)
 # no need to handle edge dofs, as underlying interpolation is <= 2D
@@ -19,9 +21,9 @@ function Ferrite.value(ip::JumpInterpolation, i::Int, ξ::Vec{dim_base}) where d
     @assert Ferrite.getdim(ip_base) == dim_base
     0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
 
-    vertex_dofs = nvertices(ip_base)*Ferrite.nvertexdofs(ip_base)
-    face_dofs = length(Ferrite.faces(ip_base))*Ferrite.nfacedofs(ip_base)
-    cell_dofs = Ferrite.ncelldofs(ip_base)
+    vertex_dofs = nvertices(ip_base)*nvertexdofs(ip_base)
+    face_dofs = length(Ferrite.faces(ip_base))*nfacedofs(ip_base)
+    cell_dofs = ncelldofs(ip_base)
     if i <= 2vertex_dofs
         if i <= vertex_dofs
             return -Ferrite.value(ip_base, i, ξ)
@@ -55,9 +57,9 @@ function Ferrite.value(ip::MidPlaneInterpolation, i::Int, ξ::Vec{dim_base}) whe
     @assert Ferrite.getdim(ip_base) == dim_base
     0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
 
-    vertex_dofs = nvertices(ip_base)*Ferrite.nvertexdofs(ip_base)
-    face_dofs = length(Ferrite.faces(ip_base))*Ferrite.nfacedofs(ip_base)
-    cell_dofs = Ferrite.ncelldofs(ip_base)
+    vertex_dofs = nvertices(ip_base)*nvertexdofs(ip_base)
+    face_dofs = length(Ferrite.faces(ip_base))*nfacedofs(ip_base)
+    cell_dofs = ncelldofs(ip_base)
     if i <= 2vertex_dofs
         if i <= vertex_dofs
             return 0.5Ferrite.value(ip_base, i, ξ)
