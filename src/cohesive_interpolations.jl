c299742a822dd19c7b6122ca6e5186178be35c72
termi-official <termi-official@users.noreply.github.com>
2023-05-24 17:45:28 +0200

    cleanup.

--- src/cohesive_interpolations.jl
+++ src/cohesive_interpolations.jl
@@ -1,6 +1,6 @@
 abstract type SurfaceInterpolation{dim,shape,order,ip} <: Interpolation{dim,shape,order} end
 
-Ferrite.getnbasefunctions(ip::SurfaceInterpolation{2, RefCube, order, Lagrange{1, RefCube, order}}) where {order} = 2*Ferrite.getnbasefunctions(ip.ip_base)
+Ferrite.getnbasefunctions(ip::SurfaceInterpolation{2, RefCube, order, Lagrange{1, RefCube, order}}) where {order} = 2*getnbasefunctions(ip.ip_base)
 Ferrite.vertexdof_indices(ip::SurfaceInterpolation{2, RefCube, order, Lagrange{1, RefCube, order}}) where {order} = ((1,),(2,),(order-1+3,),(order-1+4,)) # TODO we should be able to do it via calling `vertexdof_indices` of `ip_base` + arithmetic
 Ferrite.facedof_interior_indices(ip::SurfaceInterpolation{2, RefCube, order, Lagrange{1, RefCube, order}}) where {order} = (ntuple(i->2+i,order-1), ntuple(i->(order-1+4)+i,order-1)) # TODO we should be able to do it via calling `facedof_indices` of `ip_base` + arithmetic 
 
@@ -18,9 +18,9 @@ JumpInterpolation(ip::Interpolation{dim,shape,order}) where {dim,shape,order} = 
 function Ferrite.value(ip::JumpInterpolation, i::Int, ξ::Vec{dim_base}) where dim_base
     (; ip_base) = ip
     @assert Ferrite.getdim(ip_base) == dim_base
-    0 < i <= Ferrite.getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
+    0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
 
-    ndofs_base = Ferrite.getnbasefunctions(ip_base)
+    ndofs_base = getnbasefunctions(ip_base)
     if i <= ndofs_base
         return -Ferrite.value(ip_base, i, ξ)
     else 
@@ -39,7 +39,7 @@ function Ferrite.value(ip::MidPlaneInterpolation, i::Int, ξ::Vec{dim_base}) whe
     (; ip_base) = ip
     @assert Ferrite.getdim(ip_base) == dim_base
     0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
-    ndofs_base = Ferrite.getnbasefunctions(ip_base)
+    ndofs_base = getnbasefunctions(ip_base)
     if i <= ndofs_base
         return 0.5Ferrite.value(ip_base, i, ξ)
     else 
