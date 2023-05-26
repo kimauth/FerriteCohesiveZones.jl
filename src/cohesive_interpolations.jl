1c287fe4f8d3165cd576e82fac551a72c515e01a
termi-official <termi-official@users.noreply.github.com>
2023-05-26 14:06:42 +0200

    Fix and format.

--- src/cohesive_interpolations.jl
+++ src/cohesive_interpolations.jl
@@ -8,38 +8,39 @@ struct JumpInterpolation{dim,shape,order,ip} <: SurfaceInterpolation{dim,shape,o
     ip_base::ip
 end
 
-nvertices(ip::Interpolation) = length(Ferrite.vertexdof_indices(ip))
 nvertexdofs(ip::Interpolation) = sum(length.(Ferrite.vertexdof_indices(ip)); init=0)
 nfacedofs(ip::Interpolation) = sum(length.(Ferrite.facedof_interior_indices(ip)); init=0)
+nfacedofs(ip::Interpolation{1}) = 0
 ncelldofs(ip::Interpolation) = length(Ferrite.celldof_interior_indices(ip))
 
 JumpInterpolation(ip::Interpolation{dim,shape,order}) where {dim,shape,order} = JumpInterpolation{dim+1,shape,order,typeof(ip)}(ip)
 
+# no need to handle edge dofs, as underlying interpolation is <= 2D
 function Ferrite.value(ip::JumpInterpolation, i::Int, ξ::Vec{dim_base}) where dim_base
     (; ip_base) = ip
     @assert Ferrite.getdim(ip_base) == dim_base
     0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
 
-    vertex_dofs = nvertices(ip_base)*nvertexdofs(ip_base)
-    face_dofs = length(Ferrite.faces(ip_base))*nfacedofs(ip_base)
-    cell_dofs = ncelldofs(ip_base)
+    vertex_dofs = nvertexdofs(ip_base)
+    face_dofs   = nfacedofs(ip_base)
+    cell_dofs   = ncelldofs(ip_base)
     if i <= 2vertex_dofs
         if i <= vertex_dofs
             return -Ferrite.value(ip_base, i, ξ)
         else
-            return Ferrite.value(ip_base, i-vertex_dofs, ξ)
+            return  Ferrite.value(ip_base, i-vertex_dofs, ξ)
         end
     elseif i - 2vertex_dofs <= 2face_dofs
-        if i-2vertex_dofs <= face_dofs
+        if i - 2vertex_dofs <=  face_dofs
             return -Ferrite.value(ip_base, i-vertex_dofs, ξ)
         else
-            return Ferrite.value(ip_base, i-vertex_dofs-face_dofs, ξ)
+            return  Ferrite.value(ip_base, i-vertex_dofs-face_dofs, ξ)
         end
     elseif i - 2vertex_dofs - 2face_dofs <= 2cell_dofs
-        if i - 2vertex_dofs - 2face_dofs <= cell_dofs
-            return -Ferrite.value(ip_base, i-vertex_dofs - face_dofs, ξ)
+        if i - 2vertex_dofs - 2face_dofs <=  cell_dofs
+            return -Ferrite.value(ip_base, i-vertex_dofs-face_dofs, ξ)
         else
-            return Ferrite.value(ip_base, i-vertex_dofs-face_dofs-cell_dofs, ξ)
+            return  Ferrite.value(ip_base, i-vertex_dofs-face_dofs-cell_dofs, ξ)
         end
     end
 end
@@ -55,10 +56,27 @@ function Ferrite.value(ip::MidPlaneInterpolation, i::Int, ξ::Vec{dim_base}) whe
     (; ip_base) = ip
     @assert Ferrite.getdim(ip_base) == dim_base
     0 < i <= getnbasefunctions(ip) || throw(ArgumentError("no shape function $i for interpolation $ip"))
-    ndofs_base = getnbasefunctions(ip_base)
-    if i <= ndofs_base
-        return 0.5Ferrite.value(ip_base, i, ξ)
-    else
-        return 0.5Ferrite.value(ip_base, i-ndofs_base, ξ)
+
+    vertex_dofs = nvertexdofs(ip_base)
+    face_dofs   = nfacedofs(ip_base)
+    cell_dofs   = ncelldofs(ip_base)
+    if i <= 2vertex_dofs
+        if i <= vertex_dofs
+            return 0.5Ferrite.value(ip_base, i, ξ)
+        else
+            return 0.5Ferrite.value(ip_base, i-vertex_dofs, ξ)
+        end
+    elseif i - 2vertex_dofs <= 2face_dofs 
+        if i-2vertex_dofs <= face_dofs 
+            return 0.5Ferrite.value(ip_base, i-vertex_dofs, ξ)
+        else
+            return 0.5Ferrite.value(ip_base, i-vertex_dofs-face_dofs, ξ)
+        end
+    elseif i - 2vertex_dofs - 2face_dofs <= 2cell_dofs 
+        if i - 2vertex_dofs - 2face_dofs <= cell_dofs 
+            return 0.5Ferrite.value(ip_base, i-vertex_dofs - face_dofs, ξ)
+        else
+            return 0.5Ferrite.value(ip_base, i-vertex_dofs-face_dofs-cell_dofs, ξ)
+        end
     end
 end
